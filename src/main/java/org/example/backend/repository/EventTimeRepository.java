ab3d2beec5f89878b5b9634049a3976e932722a5
林承亮 <149670220+DyingCoderLin@users.noreply.github.com>
2024-05-04 00:04:40 +0800

    Revert "Backend dev"

--- src/main/java/org/example/backend/repository/EventTimeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.example.backend.model.EventTime;
-
-@Repository
-public interface EventTimeRepository extends JpaRepository<EventTime, Integer> {
-}
